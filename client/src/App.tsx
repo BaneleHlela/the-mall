0c433e29f820fe3c2c1ef235c92a6277b5a9aa02
BaneleHlela <gizahlela@gmail.com>
2025-04-24 19:10:23 +0200

    Returned the scribbler component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LayoutsSection from './pages/website-builder/LayoutsSection.tsx';
+import ScribblerComponent from "./components/ScribblerComponent.tsx";
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/*" element={<LayoutsSection />} />
-        {/* Removed ScribblerComponent route */}
+        <Route path="/scribbler" element={<ScribblerComponent />} />
       </Routes>
     </Router>
   );
